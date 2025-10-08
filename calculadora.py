9a4c25c4442c679451fa3cc4daae3fa0774f703c
HENRIQUE ZANCHET SANTANA <90800843+HenriqueZanchet@users.noreply.github.com>
2025-10-08 19:07:01 +0000

    FEAT: add funcao soma

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,9 @@
 # calculadora.py
 
+def somar(a, b):
+    """Retorna a soma de dois números."""
+    return a + b
+
 def main():
     """Função principal do programa."""
     print("Bem-vindo à Calculadora em Python!")
