0d52636e28a9d0fd535b6b736a79debc59ab9909
HENRIQUE ZANCHET SANTANA <90800843+HenriqueZanchet@users.noreply.github.com>
2025-10-09 19:18:59 +0000

    FEAT: add interacao do usuario com funcao_soma

--- calculadora.py
+++ calculadora.py
@@ -8,5 +8,11 @@ def main():
     """Função principal do programa."""
     print("Bem-vindo à Calculadora em Python!")
 
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    resultado = somar(num1, num2)
+    print(f"A soma de {num1} e {num2} é: {resultado}")
+
 if __name__ == "__main__":
     main()
