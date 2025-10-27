f46675add013ea8d11a2e97a13511c4bc73423f2
HENRIQUE ZANCHET SANTANA <90800843+HenriqueZanchet@users.noreply.github.com>
2025-10-27 17:28:11 -0300

    GITHUB: Modify subtrair function to handle three arguments

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 # calculadora.py
-def subtrair(a, b):
-    """Retorna a subtração de dois números."""
-    return a - b
+def subtrair(a, b, c):
+    """Retorna a subtração de três números."""
+    return a - b - c
 
 
 def somar3(a, b, c):
@@ -22,8 +22,9 @@ def main():
     print("Agora vamos subtrair dois números.")
     num4 = float(input("Digite o primeiro número: "))
     num5 = float(input("Digite o segundo número: "))
-    resultado = subtrair(num4, num5)
-    print(f"A subtração de {num4} e {num5} é: {resultado}")
+    num6 = float(input("Digite o terceiro número: "))
+    resultado = subtrair(num4, num5, num6)
+    print(f"A subtração de {num4} e {num5} e {num6} é: {resultado}")
 
     print("Fim da aplicação.")
 
